164ad819f13d5d8338443780e1ed24fd69eb0bd4
EC2 Default User <ec2-user@ip-172-31-44-45.ap-south-1.compute.internal>
2023-01-08 11:10:11 +0000

    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, this is time work hardy");
+    System.out.println("Hello,no tinrer this is time work hardy");
+    
   }
 }
